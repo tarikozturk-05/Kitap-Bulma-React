4b78a90fb2caf91e5831484e46aee8b6b4071401
Tarik <a.tarik2005@gmail.com>
2022-11-01 13:00:35 +0300

    fixedfixed

--- src/Home.jsx
+++ src/Home.jsx
@@ -12,15 +12,19 @@ const [items, setItems] = useState([])
     const url = `https://www.googleapis.com/books/v1/volumes?q=%27${bookName}%27&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU&maxResults=25`
 
     const ApiGel =async()=>{
+      
         // if(e.key === "Enter"){
         //     handleSubmit()
         //     console.log("entercalistii")
         // }   
 
             try {
+              
                const {data} = await axios(url)
                 console.log(data.items);
                 setItems(data.items)
+                
+               
             } catch (error) {
                 console.log(error);
             }
@@ -38,28 +42,37 @@ setBookName("")
 
 
  return (
-    <div>
-        <form action="" onSubmit={handleSubmit}>
+    <div className='formm'>
+        <form  action="" onSubmit={handleSubmit}>
 
             <h1>Kitap Bul</h1> <br />
 
-            <input type="text" value={bookName} onChange={(e)=>setBookName(e.target.value)} placeholder='Kitap Ismi Gir...' />
+            <input  type="text" value={bookName} onChange={(e)=>setBookName(e.target.value)} placeholder='Kitap Ismi Gir...' />
 
             <button type='submit'>Ok</button>
             </form>
 
 
-            <div>
+            <div className='kapsayan'>
                 {
-                    items.map((ahmet)=>{
-                        return(
-                            <div>
-                                <img src={ahmet?.volumeInfo?.imageLinks?.thumbnail} alt="" />
-                                    
-
-                            </div>
-
-                        )
+                    items.map((item,index)=>{
+                        return (
+                            item?.volumeInfo?.imageLinks ?
+                              (<div key={index} className="cardd">
+                                <img
+                                  src={
+            
+                                    item?.volumeInfo.imageLinks?.smallThumbnail
+                                  }
+                                  alt="empty"
+                                />
+                                <h4>
+                                  {item?.volumeInfo.imageLinks &&
+                                    item?.volumeInfo.imageLinks &&
+                                    item?.volumeInfo?.title}{" "}
+                                </h4>
+                              </div>) : ""
+                            );
                     })
                 }
 
